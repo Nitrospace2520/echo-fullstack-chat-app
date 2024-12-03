9f0d53d4140353733f1d909bb5c83ef068e1e256
Nirmalya <nirmalyadhara18@gmail.com>
2024-12-03 19:21:34 +0530

    update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,8 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 import { axiosInstance } from "../lib/axios";
 
-const BASE_URL = import.meta.env.MODE==="development" ? "http://localhost:5001" : "/",
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 // note: this is a simple store that only stores the authUser and isCheckingAuth
 export const useAuthStore = create((set, get) => ({
